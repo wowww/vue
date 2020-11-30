aa07eebb124ece7e0767ade2314d4c93cd305272
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-12-01 05:27:20 +0900

    style. TodoHeader component

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,13 @@
 <template>
-  <div>
-    <header></header>
-  </div>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
 </template>
 
-<script>
-export default {
-
+<style scoped>
+h1 {
+  color: #2F3852;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
 }
-</script>
-
-<style>
-
 </style>
