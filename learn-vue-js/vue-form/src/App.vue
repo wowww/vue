dbe4f19664f7f7c89211bf8d8ce501157e2abb18
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-12-01 02:53:12 +0900

    feat. 프로젝트 생성 및 마크업 작업

---
+++ learn-vue-js/vue-form/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <form action="">
+      <div>
+        <label for="username">id: </label>
+        <input id="username" type="text">
+      </div>
+      <div>
+        <label for="password">pw: </label>
+        <input id="password" type="password">
+      </div>
+    </form>
+    <button>login</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
