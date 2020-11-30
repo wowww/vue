2971026febd1309517285c42d977b8c2ef2a1ddf
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-12-01 02:30:05 +0900

    feat. 싱글 파일 컴포넌트에서 props 속성 사용하는 방법

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <app-header></app-header>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str"></app-header>
   </div>
 </template>
 
@@ -8,11 +9,11 @@
 import AppHeader from './components/AppHeader.vue';
 
 export default {
-  // data: function() {
-  //   return {
-  //     str: 'hi'
-  //   }
-  // },
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
   components: {
     'app-header': AppHeader
   } 
