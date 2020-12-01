e2947ffb566a1d162fb0a14d933ad8ecdca6884c
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-12-01 20:59:31 +0900

    refactor. 할 일 모두 삭제 기능

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -6,7 +6,7 @@
     v-on:removeItem="removeOneItem" 
     v-on:toggleItem="toggleOneItem">
     </TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
   </div>
 </template>
 
@@ -38,6 +38,10 @@ export default {
       this.todoItems[index].completed = !this.todoItems[index].completed
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAllItems: function() {
+      localStorage.clear();
+      this.todoItems = [];
     }
   },
   created: function() {
--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -8,7 +8,7 @@
 export default {
   methods: {
     clearTodo: function() {
-      localStorage.clear();
+      this.$emit('clearAll');
     }
   }
 }
@@ -29,5 +29,4 @@ export default {
   display: block;
   color: #e20303;
 }
-
 </style>
