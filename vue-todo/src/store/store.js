fb306402d968d071ff18f8cf892d81ae6009b8bc
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-12-03 03:13:32 +0900

    refactor. state 속성 적용

--- vue-todo/src/store/store.js
+++ vue-todo/src/store/store.js
@@ -3,8 +3,22 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
+const storage = {
+  fetch() {
+    const arr = [];
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+          arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
+    return arr;
+  },
+}
+
 export const store = new Vuex.Store({
   state: {
-    // headerText: 'TODO it!'
+    todoItems: storage.fetch()
   }
 });
